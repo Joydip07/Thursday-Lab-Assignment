f913d4fe601cdeda4ce50fac9a6585a730a5c5e7
Joydip07 <106582977+Joydip07@users.noreply.github.com>
2022-06-24 10:35:12 +0530

    Add files via upload

--- LibraryInterface.java
+++ LibraryInterface.java
@@ -5,53 +5,53 @@ package OOps.inheritence;
 //interface
 interface LibraryUser
 {
-	void registerAccount();
-	void requestBook();
+void registerAccount();
+void requestBook();
 }// end of library user interface
 class KidUsers implements LibraryUser
 {
-	int age;
-	String bookType;
+private int age;
+private String bookType;
 	
 
     
-	public KidUsers(int age, String bookType) {
-		this.age = age;
-		this.bookType = bookType;
-	}
+public KidUsers(int age, String bookType) {
+this.age = age;
+this.bookType = bookType;
+}
 		
 	
 
-	@Override
-	public void registerAccount() {
-		if (age<12)
-		{
-			System.out.println("You have succesfully registered under kids account");
-		}
-		else
-		{
-			System.out.println("Sorry,age must be less than 12 to register as a kid");
-		}
+@Override
+public void registerAccount() {
+if (age<12)
+{
+System.out.println("You have succesfully registered under kids account");
+}
+else
+{
+System.out.println("Sorry,age must be less than 12 to register as a kid");
+}
 		
 		
 		
-	}
+}
 
-	@Override
-	public void requestBook() {
-		if (bookType=="Kids") 
-		{
-			System.out.println("Book issued successfully");
-			System.out.println("Please return the book within 10 days");
-		}
-		else
-		{
-			System.out.println("oops,you are allowed to take only kids book");
-		}
+@Override
+public void requestBook() {
+if (bookType=="Kids") 
+{
+System.out.println("Book issued successfully");
+System.out.println("Please return the book within 10 days");
+}
+else
+{
+System.out.println("oops,you are allowed to take only kids book");
+}
 	
 		
 		
-	}
+}
 
 	
 	
@@ -59,65 +59,66 @@ class KidUsers implements LibraryUser
 
 class AdultUser implements LibraryUser
 {
-	int age;
-	String bookType;
+private int age;
+private String bookType;
 	
 
 
-	public AdultUser(int age, String bookType) {
+public AdultUser(int age, String bookType) {
 	
-		this.age = age;
-		this.bookType = bookType;
-	}
+this.age = age;
+this.bookType = bookType;
+}
 
 
-	@Override
-	public void registerAccount() {
+@Override
+public void registerAccount() {
 		
 		
 		 
-		if (age>12)
-		{
-			System.out.println("You have succesfully registered as an Adult Account");
-		}
-		else
-		{
-			System.out.println("Sorry Age must be greater than 12 to register as an adult");
-		}
+if (age>12)
+{
+System.out.println("You have succesfully registered as an Adult Account");
+}
+else
+{
+System.out.println("Sorry Age must be greater than 12 to register as an adult");
+}
 			
-	}
+}
 
-	@Override
-	public void requestBook() 
-	{
+@Override
+public void requestBook() 
+{
 
 
 
-		if (bookType=="Fiction")
-		{
-			System.out.println("Book Issued successfully,please return the book within 7 days");
+if (bookType=="Fiction")
+{
+System.out.println("Book Issued successfully,please return the book within 7 days");
 			
-		}
-		else
-		{
-			System.out.println("Oops you arfe allowed to take only adult Fiction books");
-		}
+}
+else
+{
+			
+System.out.println("Oops you arfe allowed to take only adult Fiction books");
+}
 
 		
 		
-	}
+}
 	
 }//end oF ADULT USER class
 
 public class LibraryInterface {
 
-	public static void main(String[] args) {
-	KidUsers ku= new KidUsers(10, "Kids");
-	ku.registerAccount();
-    ku.requestBook();
-    AdultUser adu= new AdultUser(20, "Fiction");
-    adu.registerAccount();
-    adu.requestBook();
+public static void main(String[] args) {
+KidUsers ku= new KidUsers(10, "Kids");
+ku.registerAccount();
+ku.requestBook();
+AdultUser adu= new AdultUser(20, "Fiction");
+adu.registerAccount();
+adu.requestBook();
 	}
 
 }
